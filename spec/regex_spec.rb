0ac47651319957fe3faa2dfba015492453c600a9
Ash Moran <ash.moran@patchspace.co.uk>
2012-05-20 00:03:07 +0100

    Single character

--- spec/regex_spec.rb
+++ spec/regex_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 
+class SimpleRegex
+  def initialize(expression)
+    @expression = expression
+  end
+
+  def matches?(string)
+    @expression[0] == string[0]
+  end
+end
+
+describe SimpleRegex do
+  context "single character" do
+    let(:regex) { SimpleRegex.new("a") }
+
+    it "matches the same character" do
+      regex.should match("a")
+    end
+    
+    it "doesn't match a different character" do
+      regex.should_not match("b")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ RSpec.configure do |config|
   config.filter_run(focus: true)
   config.run_all_when_everything_filtered = true
 end
+
+RSpec::Matchers.define :match do |expected|
+  match do |actual|
+    actual.matches?(expected)
+  end
+end
