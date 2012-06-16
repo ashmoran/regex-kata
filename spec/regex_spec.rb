dbc5c7e63f5b40aa13c6db354f63574c1df03a32
Ash Moran <ash.moran@patchspace.co.uk>
2012-06-16 17:50:13 +0100

    Simple strings

--- spec/regex_spec.rb
+++ spec/regex_spec.rb
@@ -6,13 +6,17 @@ class SimpleRegex
   end
 
   def matches?(string)
-    @expression[0] == string[0]
+    @expression.chars.each_with_index do |element, index|
+      return false unless element == string[index]
+    end
   end
 end
 
 describe SimpleRegex do
+  let(:regex) { SimpleRegex.new(regex_body) }
+
   context "single character" do
-    let(:regex) { SimpleRegex.new("a") }
+    let(:regex_body) { "a" }
 
     it "matches the same character" do
       regex.should match("a")
@@ -20,6 +24,26 @@ describe SimpleRegex do
     
     it "doesn't match a different character" do
       regex.should_not match("b")
+    end
+  end
+
+  context "multiple characters" do
+    let(:regex_body) { "abc" }
+
+    it "matches the same character" do
+      regex.should match("abc")
+    end
+    
+    it "doesn't match a different character" do
+      regex.should_not match("axc")
+    end
+
+    it "matches a string that contains extra characters at the end" do
+      regex.should match("abcd")
+    end
+    
+    it "doesn't match a string that is too short" do
+      regex.should_not match("ab")
     end
   end
 end
